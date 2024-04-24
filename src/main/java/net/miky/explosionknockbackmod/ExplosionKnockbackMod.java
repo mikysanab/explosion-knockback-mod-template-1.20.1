acafdff1c90cf378e6c48cb3018852525944b92c
Michael Sanabria <mikysanab@gmail.com>
2024-04-24 01:17:03 -0700

    Initial commit

--- src/main/java/net/miky/explosionknockbackmod/ExplosionKnockbackMod.java
+++ src/main/java/net/miky/explosionknockbackmod/ExplosionKnockbackMod.java
@@ -3,8 +3,8 @@ import net.fabricmc.api.ModInitializer;
 
 public class ExplosionKnockbackMod implements ModInitializer {
 
-	public static final String MOD_ID = "explosionknockbackmod";
-	//public static final Logger LOGGER = LoggerFactory.getLogger("explosion-knockback-mod");
+	// public static final String MOD_ID = "explosionknockbackmod";
+	// public static final Logger LOGGER = LoggerFactory.getLogger("explosion-knockback-mod");
 
 	@Override
 	public void onInitialize() {
