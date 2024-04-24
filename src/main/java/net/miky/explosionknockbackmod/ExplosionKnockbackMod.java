33c7fbe6b0f5ee36956c2a72b112aef8f858f8f4
Michael Sanabria <mikysanab@gmail.com>
2024-04-24 01:16:33 -0700

    Initial commit

--- src/main/java/net/miky/explosionknockbackmod/ExplosionKnockbackMod.java
+++ src/main/java/net/miky/explosionknockbackmod/ExplosionKnockbackMod.java
@@ -1,7 +1,5 @@
 package net.miky.explosionknockbackmod;
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ExplosionKnockbackMod implements ModInitializer {
 
